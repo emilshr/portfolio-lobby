01be70905aa5cf4255ddbf3227d5126d34f74205
Emil Sharier <emil.sharier.dev@gmail.com>
2025-01-18 13:36:26 +0530

    wip

--- backend/middlewares/cors.go
+++ backend/middlewares/cors.go
@@ -2,13 +2,18 @@ package middleware
 
 import (
 	"net/http"
+	"portfolio/lobby/constants"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		if constants.ENV == "local" {
+			context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		} else {
+			context.Writer.Header().Set("Access-Control-Allow-Origin", "emilshr.com")
+		}
 		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")

55f0ec7e20a936a876597f2ac0311d78f2f92b5c
Emil Sharier <emil.sharier.dev@gmail.com>
2025-01-18 15:23:27 +0530

    wip

--- backend/routes/login.go
+++ backend/routes/login.go
@@ -32,7 +32,7 @@ func Login(context *gin.Context) {
 	}
 
 	context.SetSameSite(http.SameSiteNoneMode)
-	context.SetCookie(constants.REFRESH_TOKEN_COOKIE, result.RefreshToken, 86400, "/", "", false, true)
+	context.SetCookie(constants.REFRESH_TOKEN_COOKIE, result.RefreshToken, 86400, "/", "", true, true)
 
 	context.JSON(http.StatusOK, gin.H{"accessToken": result.AccessToken, "username": result.Username})
 

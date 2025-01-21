a0228e44d590c883bcce043f4661dbcced0ab3e2
Emil Sharier <emil.sharier.dev@gmail.com>
2025-01-21 21:26:39 +0530

    feat: Optimizing imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,6 @@ import { ThemeProvider } from "./components/theme-provider.tsx";
 import { BrowserRouter } from "react-router";
 import { RoutesWrapper } from "./Routes.tsx";
 import { Navbar } from "./components/Navbar.tsx";
-
-import "@fontsource/inter/100-italic.css";
-import "@fontsource/inter/200-italic.css";
-import "@fontsource/inter/300-italic.css";
-import "@fontsource/inter/400-italic.css";
-import "@fontsource/inter/500-italic.css";
-import "@fontsource/inter/600-italic.css";
-import "@fontsource/inter/700-italic.css";
-import "@fontsource/inter/800-italic.css";
-import "@fontsource/inter/900-italic.css";
 import { QueryClientWrapper } from "./components/query-client-wrapper.tsx";
 import { AuthInit } from "./auth/auth-init.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
